6e6049fbe6ede09135dcc7701c7fbd05ddfb80f7
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2023-05-04 14:20:13 +0600

    generic-constraints

--- generic-constraints.ts
+++ generic-constraints.ts
@@ -1,14 +1,35 @@
-const addMeToMyCrushMind = <T>(myInfo: T) => {
+
+type MandatoryInterface {
+    name: string,
+    age: number,
+    salary: number
+}
+
+
+const addMeToMyCrushMind = <T extends MandatoryInterface>(myInfo: T) => {
     const crush = 'kate Winslate';
     const newData = { ...myInfo, crush };
     return newData;
 
 }
-const myInfo = {
-    name: "Rahat",
-    age: 28,
-    salary: 1000000
+
+type MyInfoType = {
+    name: string,
+    age: number,
+    salary: number,
+    wife: boolean,
+    other: null
 }
-const result5 = addMeToMyCrushMind(myInfo);
+const myInfo :MyInfoType = {
+    name: "Kate",
+    age: 28,
+    salary: 1000000,
+    wife: true,
+    other:null
+}
+const result5 = addMeToMyCrushMind<MyInfoType>(myInfo);
+const result6 = addMeToMyCrushMind({ monerKhusi: "hashi khushi" });
+
+
 
 console.log(result5);
